85e1e3b35f1673d71e0eb945d0a13f1ea85e93a2
ardeness <soohyun.park@hyundai-autoever.com>
2018-05-03 17:02:01 +0900

    - Show "I am listening" gif animation on modal with fade in/out animation - Add websocket module(localhost:8765)

--- src/App.js
+++ src/App.js
@@ -1,28 +1,57 @@
 import React, { Component } from 'react';
-
 import GifPlayer from 'react-gif-player';
-
-import siri_like from './siri_like.gif';
-import ai_listening from './ai_listening.gif';
-
+import Rodal from 'rodal';
+import Websocket from 'react-websocket';
+import 'rodal/lib/rodal.css';
 import './App.css';
 
+import ai_listening from './ai_listening.gif';
 import Clock from './Clock';
 import Weather from './Weather';
 
-class App extends Component {
-  componentDidMount() {
+const modalStyle = {
+    backgroundColor: 'black'
+}
 
+class App extends Component {
+
+  constructor(props) {
+    super(props);
+    this.state = { visible: false };
+  }
+
+  componentDidMount() {
+  }
+
+  messageHandler = (msg) => {
+    console.log(msg);
+  }
+  show = () => {
+    this.setState({visible:true});
+  }
+
+  hide = () => {
+    this.setState({visible:false});
   }
 
   render() {
     return (
       <div>
+        <Websocket url='ws://localhost:8765/' onMessage={this.messageHandler}/>
         <Clock/>
         <Weather/>
-	<div style={{width:'100%', display: 'flex', justifyContent:'center'}}>
-          <GifPlayer gif={ai_listening} autoplay={true} width={100}/>
-	</div>
+        <Rodal
+          animation={'fade'}
+          visible={this.state.visible}
+          onClose={this.hide}
+          duration={1000}
+          customStyles={modalStyle}
+          showCloseButton={false}
+        >
+  	      <div style={{width:'100%', display: 'flex', justifyContent:'center'}}>
+            <GifPlayer gif={ai_listening} autoplay={true} width={200}/>
+  	      </div>
+        </Rodal>
       </div>
     );
   }
