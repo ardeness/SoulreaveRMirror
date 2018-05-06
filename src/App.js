69b6600708aaee89775deb7a2d4592379e346af4
Soohyun Park <ardeness@gmail.com>
2018-05-06 23:44:45 +0900

    Fix what?

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import React, { Component } from 'react';
 import GifPlayer from 'react-gif-player';
 import Rodal from 'rodal';
 import Websocket from 'react-websocket';
+import YouTube from 'react-youtube';
+
 import 'rodal/lib/rodal.css';
 import './App.css';
 
@@ -17,7 +19,7 @@ class App extends Component {
 
   constructor(props) {
     super(props);
-    this.state = { visible: false, request: '' };
+    this.state = { visible: false, request: '', musicPlayer: undefined };
   }
 
   componentDidMount() {
@@ -32,6 +34,18 @@ class App extends Component {
     else if(response.status === 'responding') {
       this.hide();
       this.setState({request:response.request});
+      if(response.request === 'play music') {
+        if(this.state.musicPlayer) this.state.musicPlayer.playVideo();
+      }      
+      else if(response.request === 'stop music') {
+        if(this.state.musicPlayer) this.state.musicPlayer.stopVideo();
+      }      
+      else if(response.request === 'pause music') {
+        if(this.state.musicPlayer) this.state.musicPlayer.pauseVideo();
+      }
+      else if(response.request === 'resume music') {
+        if(this.state.musicPlayer) this.state.musicPlayer.playVideo();
+      }
     }
   }
 
@@ -47,7 +61,30 @@ class App extends Component {
     this.setState({visible:false});
   }
 
+  youtubeOnReady = event => {
+    event.target.setVolume(40);
+    event.target.playVideo();
+    this.setState({musicPlayer: event.target});
+  }
+
   render() {
+    const opts= {
+      width: '600',
+      height: '338',
+      playerVars: { 
+        autoplay: 1,
+        listType: 'search',
+        list: 'jazz',
+        controls: 0,
+        disablekb: 1,
+        fs: 0,
+        playsinline: 1,
+        rel: 0,
+        showinfo: 0,
+        color: 'white'
+      }
+    };
+
     return (
       <div>
         <Websocket url='ws://127.0.0.1:8765/' onMessage={this.messageHandler} onOpen={this.onOpen} />
@@ -65,6 +102,12 @@ class App extends Component {
             <GifPlayer gif={ai_listening} autoplay={true} width={200}/>
           </div>
         </Rodal>
+        <div style={{width:'100%', display: 'none', justifyContent:'center'}}>
+          <YouTube
+            opts={opts}
+            onReady={this.youtubeOnReady}   
+          />
+        </div>
         <div>{this.state.request}</div>
       </div>
     );
